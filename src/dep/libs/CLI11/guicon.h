909bbcf3e973e9ab72537c160dc51b05764deba1
Andrew A. Ritz <drew@august-harper.com>
2025-06-09 22:27:06 -0400

    - Added some missing files for the CLI stuff.

---
+++ src/dep/libs/CLI11/guicon.h
@@ -0,0 +1,11 @@
+// NOTE: Copied from: 
+// https://stackoverflow.com/questions/191842/how-do-i-get-console-output-in-c-with-a-windows-program
+
+#ifndef __GUICON_H__
+#define __GUICON_H__
+#ifdef _DEBUG
+
+void RedirectIOToConsole();
+
+#endif
+#endif
